39b711fb2c19c2bf7ac8efb19c5419dce2631849
Marcus Jaschen <mjaschen@gmail.com>
2018-10-12 17:46:14 +0200

    display kudos count in bot reply; localize answer

--- kudos.py
+++ kudos.py
@@ -25,20 +25,22 @@ class Kudos(BotPlugin):
             'kudos': new_count,
         }
 
+        return new_count
+
     @re_botcmd(pattern=r'[\w-]+\+\+', prefixed=False, flags=re.IGNORECASE)
     def give_kudos(self, msg, match):
         """This gives kudos"""
         if match:
             line = match.group(0)
-            username = line.split(' ')[0].rstrip('++')
-            self.update_kudos(username)
+            username = line.split(' ')[0].rstrip('++').lower()
+            kudos_count = self.update_kudos(username)
 
             t = msg.frm.room if msg.is_group else msg.frm
             self.send(t,
-                      'kudos updated for {}'.format(username),
+                      'Yes! {} hat nun {} Kudos!'.format(username.title(), kudos_count),
                       in_reply_to=msg,
                       groupchat_nick_reply=True)
- 
+
     @botcmd(admin_only=True)
     def kudos_delete_entries(self, msg, args):
         """Deletes all entries for a user"""
